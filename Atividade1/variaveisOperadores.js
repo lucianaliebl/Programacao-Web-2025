e789fd3bc7fe74270a0751644ac64dabf1b23e40
lucianaliebl <142036250+lucianaliebl@users.noreply.github.com>
2025-02-25 11:04:26 -0300

    Create variaveisOperadores.js

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,18 @@
+//Adição 
+let x = 10;
+let y = 5; 
+let resultado = x + y;
+
+console.log(`O resultado da adição de x = ${x} mais y = ${y} é igual a ${resultado}`);
+
+//Subtração
+resultado = x - y;
+console.log(`O resultado da subtração de x = ${x} menos y = ${y} é igual a ${resultado}`);
+
+//Multiplicação 
+resultado = x * y;
+console.log(`O resultado da multiplicação de x = ${x} vezes y = ${y} é igual a ${resultado}`);
+
+//Divisão
+resultado = x/y;
+console.log(`O resultado da divisão de x = ${x} dividido y = ${y} é igual a ${resultado}`);
