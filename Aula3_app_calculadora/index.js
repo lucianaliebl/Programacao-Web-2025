1253e3c60c2e70253453b643164fa2a805a63496
lucianaliebl <lucianaliebl@gmail.com>
2025-03-18 20:05:40 -0300

    add

--- Aula3_app_calculadora/index.js
+++ Aula3_app_calculadora/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) =>{
     res.send('Olá mundo!')
 });
 
+app.get('/ola/:nome', (req, res)=>{
+    res.send(`Ólá, ${req.params.nome}!`)
+});
+
 const PORT = 8080;
 app.listen(PORT, () =>{
     console.log('app rodando na porta ' + PORT);
