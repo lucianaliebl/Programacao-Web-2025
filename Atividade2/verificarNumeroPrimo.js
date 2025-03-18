13495e9607c36bff5fc4feeaa45fcc57acfc5628
lucianaliebl <lucianaliebl@gmail.com>
2025-03-17 22:17:38 -0300

    add alterações

--- Atividade2/verificarNumeroPrimo.js
+++ Atividade2/verificarNumeroPrimo.js
@@ -11,21 +11,12 @@ function verificarNumeroPrimo(n){
 }
 
 console.log(`verificarNumeroPrimo(${0}) = ${verificarNumeroPrimo(0)}`);
-
 console.log(`verificarNumeroPrimo(${1}) = ${verificarNumeroPrimo(1)}`);
-
 console.log(`verificarNumeroPrimo(${2}) = ${verificarNumeroPrimo(2)}`);
-
 console.log(`verificarNumeroPrimo(${3}) = ${verificarNumeroPrimo(3)}`);
-
 console.log(`verificarNumeroPrimo(${7}) = ${verificarNumeroPrimo(7)}`);
-
 console.log(`verificarNumeroPrimo(${83}) = ${verificarNumeroPrimo(83)}`);
-
 console.log(`verificarNumeroPrimo(${100}) = ${verificarNumeroPrimo(100)}`);
-
 console.log(`verificarNumeroPrimo(${991}) = ${verificarNumeroPrimo(991)}`);
-
 console.log(`verificarNumeroPrimo(${104729}) = ${verificarNumeroPrimo(104729)}`);
-
 console.log(`verificarNumeroPrimo(${14348907}) = ${verificarNumeroPrimo(14348907)}`);
