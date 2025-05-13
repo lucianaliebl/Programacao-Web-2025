20ce02887ffd43a6181f2b258fc5e73442360999
lucianaliebl <lucianaliebl@gmail.com>
2025-05-13 10:14:07 -0300

    add

--- 6_Aula6/index.js
+++ 6_Aula6/index.js
@@ -7,6 +7,10 @@ app.engine ('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname +'/views');
 
+app.get('/', (req, res)=>{
+    res.render('index.html');
+})
+
 const PORT = 8080;
 app.listen(PORT,() =>{
     console.log(`app rodando na porta ` + PORT);
